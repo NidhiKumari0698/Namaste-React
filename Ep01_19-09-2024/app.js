130c767ce34a1f651708714d6209467820ab909a
NidhiKumari0698 <nidhi.singh.0698@gmail.com>
2024-09-19 12:21:27 +0530

    feat: Inception of react

---
+++ Ep01_19-09-2024/app.js
@@ -0,0 +1,64 @@
+// <div id="parent">
+//     <div id="child">
+//         <h1>I am h1 tag</h1>
+//         <h2>I am h2 tag</h2>
+//     </div>
+//     <div id="child2">
+//         <h1>I am h1 tag</h1>
+//         <h2>I am h2 tag</h2>
+//     </div>
+// </div>
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+    style: { backgroundColor: "green", height: "200px", width: "200px" },
+  },
+  [
+    React.createElement(
+        "div",
+        {
+          id: "child",
+          style: { backgroundColor: "orange", height: "170px", width: "170px" },
+        },
+        [
+          React.createElement(
+            "h1",
+            { style: { backgroundColor: "blue", height: "100px", width: "100px" } },
+            "I am h1 tag"
+          ),
+          React.createElement(
+            "h2",
+            { style: { backgroundColor: "blue", height: "100px", width: "100px" } },
+            "I am h2 tag"
+          ),
+        ]
+      ),
+      React.createElement(
+        "div",
+        {
+          id: "child2",
+          style: { backgroundColor: "orange", height: "170px", width: "170px" },
+        },
+        [
+          React.createElement(
+            "h1",
+            { style: { backgroundColor: "blue", height: "100px", width: "100px" } },
+            "I am h1 tag"
+          ),
+          React.createElement(
+            "h2",
+            { style: { backgroundColor: "blue", height: "100px", width: "100px" } },
+            "I am h2 tag"
+          ),
+        ]
+      )
+  ]
+);
+console.log("parent::", parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log("root::", root);
+
+root.render(parent);
